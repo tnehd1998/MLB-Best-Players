53242250efb4956e689287b12a3adaee22611cbb
이수동 <tnehd1998@gmail.com>
2022-04-04 12:25:25 +0900

    fix: MainPage 선수 데이터 SSG로 수정

--- pages/api/ranking.ts
+++ pages/api/ranking.ts
@@ -4,9 +4,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const baseUrl = process.env.NEXT_APP_BASE_URL;
 
-  const response = await axios(`${baseUrl}/ranking`);
-
-  res.status(200).json(response.data);
+  try {
+    const response = await axios(`${baseUrl}/ranking`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 }
 
 export default handler;
