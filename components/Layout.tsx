0a26e3760dc116fa27f4e2505af19228a594f311
이수동 <tnehd1998@gmail.com>
2022-04-08 14:41:06 +0900

    feat: Like 페이지 생성 및 Layout 메뉴에 추가

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -108,6 +108,25 @@ const Layout = ({ title, goBackBtn, children }: IProps) => {
             <span>Prospect</span>
           </a>
         </Link>
+        <Link href="/like">
+          <a className="flex flex-col items-center space-y-2 justify-center hover:scale-125 cursor-pointer transition-all">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+            <span>Like</span>
+          </a>
+        </Link>
       </nav>
     </div>
   );
---
+++ pages/like/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+const LikePage = () => {
+  return (
+    <Layout title="My Favorite Players">
+      <h1>Player Page</h1>
+    </Layout>
+  );
+};
+
+export default LikePage;
