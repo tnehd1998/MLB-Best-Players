f6c08d4d32c6e8916d29fbc839c9da0ec5ce4c24
이수동 <tnehd1998@gmail.com>
2022-04-12 12:35:12 +0900

    refactor: Layout 컴포넌트 menu 부분 LayoutMenus 컴포넌트로 분리

---
+++ components/LayoutMenus.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import LayoutMenu from "./LayoutMenu";
+
+const LayoutMenus = () => {
+  return (
+    <nav className="bg-white text-gray-700 border-t h-24 fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-evenly text-xs">
+      <LayoutMenu routingPath="/" menuType="Top 100">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </LayoutMenu>
+      <LayoutMenu routingPath="/prospect" menuType="Prospect">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+        />
+      </LayoutMenu>
+      <LayoutMenu routingPath="/award" menuType="Award">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+        />
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+        />
+      </LayoutMenu>
+      <LayoutMenu routingPath="/like" menuType="Like">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+        />
+      </LayoutMenu>
+    </nav>
+  );
+};
+
+export default LayoutMenus;
