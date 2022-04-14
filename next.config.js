e9d678372bd8a43045312ae2c01fe570d8dafb8d
이수동 <tnehd1998@gmail.com>
2022-04-14 10:41:57 +0900

    fix: Production 경로 수정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: isProd ? "" : "http://localhost:3000",
+  assetPrefix: isProd ? "127.0.0.1:80" : "http://localhost:3000",
   reactStrictMode: true,
   images: {
     domains: ["d1dglpr230r57l.cloudfront.net"],
