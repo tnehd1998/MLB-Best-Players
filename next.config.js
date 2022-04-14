400302e0ed5326c502de5b61c80663b87a15fe6f
이수동 <tnehd1998@gmail.com>
2022-04-14 10:37:46 +0900

    fix: Production 경로 수정

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: isProd
-    ? "https://mlb-super-stars.vercel.app"
-    : "http://localhost:3000",
+  assetPrefix: isProd ? "" : "http://localhost:3000",
   reactStrictMode: true,
   images: {
     domains: ["d1dglpr230r57l.cloudfront.net"],
